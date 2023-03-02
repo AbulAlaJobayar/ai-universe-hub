fe2daab88691665876e67436e825073d25c42264
abul-ala-jobayar-1 <jobayar59@gmail.com>
2023-03-03 00:59:38 +0600

    card-footer-end

--- js/app.js
+++ js/app.js
@@ -7,27 +7,45 @@ const sixCard=()=>{
 }
 // first ui card
 const uiSixCard = data=>{
-    console.log(data)
+    // console.log(data)
     const card = document.getElementById('card');
     data.forEach(singleData => {
-      const {image} =singleData;
+    console.log(singleData)
+
+      const {image, features,published_in,name}=singleData;
+
       const div = document.createElement('div');
       
       div.classList.add='col';
       div.innerHTML=`
     <div class="card">
         <img src="${image}" class="card-img-top img-fluid" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Features
-            
-            
-            </h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in
-                to additional content. This content is a little bit longer.</p>
-        </div>
-    </div>
+        <div class="card-body border-bottom">
+            <div>
+                <h4 class="card-title">Features</h4>
+                <ol type="number">
+                  <li>${features[0]? features[0]:'No Feather'}</li>
+                  <li>${features[1]? features[1]:'No Feather'}</li>
+                  <li>${features[2]? features[2]:'Text generation'}</li>
+                </ol>
+             </div>
+             <div class="card-footer d-flex justify-content-between align-items-center">
+                <div>
+                    <h5 class="mb-3">${name}</h5>
+                    <div class="d-flex flex-row                  align-items-center  gap-2">
+                        <i class="fa-solid fa-calendar-days"></i>
+                        <p class="m-0">${published_in}</P>
+                    </div>
+                </div>
+                <div>
+                <i class="fa-solid fa-arrow-right text-danger"></i>
+                </div>
+
+            </div>
+    </div>        
       ` 
-      card.appendChild(div) 
+      card.appendChild(div)
+      
     });
 }
 sixCard()
