1edf815518f32a7251789bdce6951fc067d4c78b
abul-ala-jobayar-1 <jobayar59@gmail.com>
2023-03-03 03:41:40 +0600

    extra section added

--- js/app.js
+++ js/app.js
@@ -69,11 +69,53 @@ const details =id=>{
     const url =`https://openapi.programming-hero.com/api/ai/tool/${id}`
     fetch(url)
     .then(res=>res.json())
-    .then(data=>modalDetails(data))
+    .then(data=>modalDetails(data.data))
     .catch(err=>console.error(err))
 }
 const modalDetails=(data)=>{
-    console.log(data)
+    console.log(data.image_link[0])
+    const{description,tool_name,pricing,features,integrations,image_link
+    }=data
+const section=document.getElementById('modal-section');
+section.innerHTML=`
+
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="btn-close outline-" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body d-flex justify-content-between align-items-center">
+                <div  class="border border-danger bg-warning-subtle  gap-2  p-4 rounded">
+                    <h5>${description}</h5>
+                    <div class="d-flex justify-content-between gap-3">
+                        <p class=" fw-bold bg-white text-success p-2">${pricing[0].price + "/ "+ pricing[0].plan ?pricing[0].price + " /"+ pricing[0].plan  :'Free of Cost/Basic'}</p>
+                        <p class="fw-bold bg-white p-2 text-warning">${pricing[1].price + " "+ pricing[1].plan ? pricing[1].price + " "+ pricing[1].plan:'Free Of Cost/Pro'}</p>
+                        <p class="bg-white p-2 text-danger fw-bold">${pricing[2].price + " "+ pricing[2].plan ? pricing[2].price + " "+ pricing[2].plan :'Free of Cost /Enterprise'}</p>
+                    </div>
+                    <div class="d-flex justify-content-between gap-5">
+                        <div >
+                            <h5>featcher</h5>
+                        </div>
+                        <div>
+                            <h5>introgation</h5>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <img src="${image_link[0]? image_link[0]: image_link[1]}" alt="">
+                    <h5>iuhgsh</h5>
+                    <p>lkfdnbln</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+`
+
 }
 
 sixCard()
